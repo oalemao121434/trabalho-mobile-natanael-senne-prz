290b9495be4d8ef83d26dcb4dcd9eaf8637786eb
joaozinho2905 <joao.anhaia.santos@escola.pr.gov.br>
2025-04-16 12:20:38 -0300

    conga2

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/Jim-Carrey-ptbr.png')}
+          source={require('@/assets/images/jim.png')}
           style={styles.reactLogo}
         />
       }>
@@ -20,29 +20,27 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Eu o Criador irei falar sobre o show de Truman.</ThemedText>
-        <ThemedText type="subtitle">Resumo:</ThemedText>
+        <ThemedText type="subtitle">Eu o Criador irei falar sobre um dos meus atores preferidos Jim Carrey.</ThemedText>
+        <ThemedText type="subtitle">informações basicas:</ThemedText>
           <ThemedText>
-          O protagonista, Truman Burbank, vive uma vida aparentemente comum em uma cidade perfeita chamada Seahaven.
-          Mas o que ele não sabe é que toda a sua vida é, na verdade, um reality show transmitido ao vivo para o mundo
-          inteiro desde o seu nascimento. Cada detalhe da sua rotina — sua casa, vizinhos, amigos e até sua esposa — é 
-          parte de um grande estúdio, com atores contratados e câmeras escondidas.
-          O criador do programa, Christof, controla tudo e justifica sua vigilância dizendo que oferece a Truman uma vida melhor do
-          que o mundo real poderia oferecer.
-          Com o tempo, Truman começa a notar inconsistências no mundo ao seu redor — coisas fora do lugar, pessoas repetindo ações,
-          comportamentos estranhos — e passa a desconfiar que há algo errado. A história se desenvolve com ele tentando entender a verdade e buscando a liberdade.<ThemedText type="defaultSemiBold">no filme acompanhamos o nosso protagonista Truman Burbank que vive uma vida aparentemente comum em uma cidade aparentemente perfeita chamada Seahaven Mas o que ele não sabe é que toda a sua vida é, na verdade, um reality show transmitido ao vivo para o mundo inteiro desde o seu nascimento.</ThemedText> Cada detalhe da sua rotina — sua casa, vizinhos, amigos e até sua esposa — é parte de um grande estúdio, com atores contratados e câmeras escondidas.{' '}
+          Nome completo: James Eugene Carrey
+          Nascimento: 17 de janeiro de 1962, em Newmarket, Ontário, Canadá
+          Nacionalidade: Canadense de nascimento, naturalizado cidadão americano em 2004
+          Família: Filho de uma dona de casa e de um músico/contador; cresceu em uma família humilde
+          Desde cedo demonstrou talento para imitações e comédia
+
+{' '}
           </ThemedText>
-        <ThemedText type="subtitle">Temas Centrais:</ThemedText>
+        <ThemedText type="subtitle">inicio da carreira</ThemedText>
         <ThemedText>
-        Controle x Liberdade: A manipulação da vida de Truman representa o controle que a mídia e as estruturas sociais podem ter sobre os indivíduos.
-        Realidade x Ilusão: O filme questiona o que é real, o que é construído e até que ponto confiamos no mundo à nossa volta.
-        Privacidade: A exposição da vida de Truman desde o nascimento levanta questões éticas sobre vigilância e entretenimento.
-        Autoconhecimento: A jornada de Truman é uma metáfora para o processo de despertar da consciência e busca pela verdade.
-          
+        Começou como comediante de stand-up aos 15 anos em clubes de Toronto
+        Passou dificuldades financeiras na juventude; sua família chegou a viver em um carro por um tempo
+        Ganhou visibilidade no final dos anos 80 com apresentações em programas de TV e shows de humor
+        "In Living Color" (1990–1994) foi um divisor de águas: lá, Jim se destacou com personagens absurdos e carismáticos, como "Fire Marshal Bill"
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">falarei um pouco do que acontece no filme</ThemedText>
+        <ThemedText type="subtitle">seus filmes de sucesso dos anos 90.</ThemedText>
         <ThemedText>
           no inicio do filme o truman esta vivendo sua vida rotineira por mais um dia quando pequenos erros de 
           continuidade despertam sua curiosidade um refletor cai do ceu bem na frente de truman escrito no refletor
@@ -83,10 +81,12 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 300, // aumentei aqui
+    width: 500,  // e aqui
     bottom: 0,
     left: 0,
     position: 'absolute',
   },
 });
+
+
